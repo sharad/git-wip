bede466bcdf00e1e43e47e6e1cea3cd6e831323b
Sharad Pratap <110339+sharad@users.noreply.github.com>
2024-12-18 15:50:24 +0530

    Added push

--- emacs/git-wip-mode.el
+++ emacs/git-wip-mode.el
@@ -1,4 +1,4 @@
-;;; git-wip-mode.el --- Use git-wip to record every buffer save
+;;; git-wip-mode.el --- Use git-wip to record every buffer save  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2013  Jerome Baum
 
@@ -27,39 +27,74 @@
   (require 'cl))
 
 (require 'vc)
+
+
+(defvar git-wip-push-commits-count 0
+  "Push to emote if no of commits exceeds git-wip-push-commits-count.")
 
 (defvar git-wip-buffer-name " *git-wip*"
   "Name of the buffer to which git-wip's output will be echoed")
 
 (defvar git-wip-path
-  (or
-   ;; Internal copy of git-wip; preferred because it will be
-   ;; version-matched
-   (expand-file-name
-    "../git-wip"
-    (file-name-directory
-     (or load-file-name
-         (locate-library "git-wip-mode"))))
-   ;; Look in $PATH and git exec-path
-   (let ((exec-path
-          (append
-           exec-path
-           (parse-colon-path
-            (replace-regexp-in-string
-             "[ \t\n\r]+\\'" ""
-             (shell-command-to-string "git --exec-path"))))))
-     (executable-find "git-wip"))))
+  (or (let ((git-wip-path (expand-file-name "../git-wip"
+                                            (file-name-directory (or load-file-name
+                                                                     (locate-library "git-wip-mode"))))))
+        ;; Internal copy of git-wip; preferred because it will be
+        ;; version-matched
+        (when (file-exists-p git-wip-path)
+          git-wip-path))
+      ;; Look in $PATH and git exec-path
+      (let ((exec-path (append exec-path
+                               (parse-colon-path
+                                (replace-regexp-in-string
+                                 "[ \t\n\r]+\\'" ""
+                                 (shell-command-to-string "git --exec-path"))))))
+        (executable-find "git-wip"))))
+
 
 (defun git-wip-after-save ()
-  (when (and (string= (vc-backend (buffer-file-name)) "Git")
+  (when (and (string= (vc-backend (buffer-file-name))
+                      "Git")
              git-wip-path)
-    (start-process "git-wip" git-wip-buffer-name
-                   git-wip-path "save" (concat "WIP from emacs: "
-                                               (file-name-nondirectory
-                                                buffer-file-name))
-                   "--editor" "--"
-                   (file-name-nondirectory buffer-file-name))
-    (message (concat "Wrote and git-wip'd " (buffer-file-name)))))
+    (let* ((git-wip-buffer (get-buffer git-wip-buffer-name))
+           (start-marker (when git-wip-buffer
+                           (with-current-buffer git-wip-buffer
+                             (buffer-end 1)))))
+      (make-process :name "git-wip"
+                    :buffer git-wip-buffer-name
+                    :command (list git-wip-path
+                                   "save"
+                                   (concat "WIP from emacs: "
+                                           (file-name-nondirectory buffer-file-name))
+                                   "--editor"
+                                   "--push" (number-to-string git-wip-push-commits-count)
+                                   "--"
+                                   (file-name-nondirectory buffer-file-name))
+                    :sentinel #'(lambda (process event)
+                                  "Sentinel to handle process exit status."
+                                  (when (string-match-p "finished\\|exited" event)
+                                    (let ((exit-code (process-exit-status process)))
+                                      (message "event %s" event)
+                                      (message "exit-code %d" exit-code)
+                                      (if (zerop exit-code)
+                                          (message (concat "Wrote and git-wip'd "
+                                                           (buffer-file-name)))
+                                        (let* ((end-marker (if git-wip-buffer
+                                                               (with-current-buffer git-wip-buffer
+                                                                 (buffer-end 1))))
+                                               (text (with-current-buffer git-wip-buffer
+                                                       (if (and start-marker
+                                                                end-marker)
+                                                           (buffer-substring start-marker end-marker)
+                                                         (when end-marker (buffer-string))))))
+                                          (message "Process %s failed with exit code %d\n%s"
+                                                   (process-name process)
+                                                   exit-code
+                                                   text)
+                                          (error "Process %s failed with exit code %d\n%s"
+                                                 (process-name process)
+                                                 exit-code
+                                                 text))))))))))
 
 ;;;###autoload
 (define-minor-mode git-wip-mode
@@ -85,7 +120,11 @@ you save a buffer."
   (when (string= (vc-backend (buffer-file-name)) "Git")
     (git-wip-mode t)))
 
-(add-hook 'find-file-hook 'git-wip-mode-if-git)
+;;;###autoload
+(add-hook 'find-file-hook
+          'git-wip-mode-if-git)
+
 
 (provide 'git-wip-mode)
-;;; git-wip-mode.el ends here
+
+  ;;; git-wip-mode.el ends here
