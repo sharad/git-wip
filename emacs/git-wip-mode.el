4ce40e3457bb3dd150a3d7100c0b82580c1c09a9
Sharad Pratap <110339+sharad@users.noreply.github.com>
2024-12-18 15:50:24 +0530

    Added push

--- emacs/git-wip-mode.el
+++ emacs/git-wip-mode.el
@@ -28,6 +28,9 @@
 
 (require 'vc)
 
+(defvar git-wip-push-commits-count 0
+  "Push to emote if no of commits exceeds git-wip-push-commits-count.")
+
 (defvar git-wip-buffer-name " *git-wip*"
   "Name of the buffer to which git-wip's output will be echoed")
 
@@ -35,57 +38,76 @@
   (or
    ;; Internal copy of git-wip; preferred because it will be
    ;; version-matched
-   (expand-file-name
-    "../git-wip"
-    (file-name-directory
-     (or load-file-name
-         (locate-library "git-wip-mode"))))
+   (let ((git-wip-path (expand-file-name "../git-wip"
+                                         (file-name-directory
+                                          (or load-file-name
+                                              (locate-library "git-wip-mode"))))))
+     (when (file-exists-p git-wip-path)
+       git-wip-path))
    ;; Look in $PATH and git exec-path
-   (let ((exec-path
-          (append
-           exec-path
-           (parse-colon-path
-            (replace-regexp-in-string
-             "[ \t\n\r]+\\'" ""
-             (shell-command-to-string "git --exec-path"))))))
+   (let ((exec-path (append exec-path
+                            (parse-colon-path
+                             (replace-regexp-in-string
+                              "[ \t\n\r]+\\'" ""
+                              (shell-command-to-string "git --exec-path"))))))
      (executable-find "git-wip"))))
 
 (defun git-wip-after-save ()
-  (when (and (string= (vc-backend (buffer-file-name)) "Git")
+  (when (and (string= (vc-backend (buffer-file-name))
+                      "Git")
              git-wip-path)
-    (start-process "git-wip" git-wip-buffer-name
-                   git-wip-path "save" (concat "WIP from emacs: "
-                                               (file-name-nondirectory
-                                                buffer-file-name))
-                   "--editor" "--"
-                   (file-name-nondirectory buffer-file-name))
-    (message (concat "Wrote and git-wip'd " (buffer-file-name)))))
+    (make-process
+     :name "git-wip"
+     :buffer git-wip-buffer-name
+     :command (list git-wip-path
+                    "save"
+                    (concat "WIP from emacs: "
+                            (file-name-nondirectory buffer-file-name))
+                    "--editor"
+                    "--push" (number-to-string git-wip-push-commits-count)
+                    "--"
+                    (file-name-nondirectory buffer-file-name))
+     :sentinel #'(lambda (process event)
+                   "Sentinel to handle process exit status."
+                   (when (string-match-p "finished\\|exited" event)
+                     (let ((exit-code (process-exit-status process)))
+                       (message "event %s" event)
+                       (message "exit-code %d" exit-code)
+                       (if (zerop exit-code)
+                           (message (concat "Wrote and git-wip'd "
+                                            (buffer-file-name)))
+                         (message "Process %s failed with exit code %d"
+                                  (process-name process)
+                                  exit-code)
+                         (error "Process %s failed with exit code %d"
+                                (process-name process)
+                                exit-code))))))))
 
-;;;###autoload
-(define-minor-mode git-wip-mode
-  "Toggle git-wip mode.
+  ;;;###autoload
+  (define-minor-mode git-wip-mode
+    "Toggle git-wip mode.
 With no argument, this command toggles the mode.
 Non-null prefix argument turns on the mode.
 Null prefix argument turns off the mode.
 
 When git-wip mode is enabled, git-wip will be called every time
 you save a buffer."
-  ;; The initial value.
-  nil
-  ;; The indicator for the mode line.
-  " WIP"
-  :group 'git-wip
+    ;; The initial value.
+    nil
+    ;; The indicator for the mode line.
+    " WIP"
+    :group 'git-wip
 
-  ;; (de-)register our hook
-  (if git-wip-mode
-      (add-hook 'after-save-hook 'git-wip-after-save nil t)
-    (remove-hook 'after-save-hook 'git-wip-after-save t)))
+    ;; (de-)register our hook
+    (if git-wip-mode
+        (add-hook 'after-save-hook 'git-wip-after-save nil t)
+      (remove-hook 'after-save-hook 'git-wip-after-save t)))
 
-(defun git-wip-mode-if-git ()
-  (when (string= (vc-backend (buffer-file-name)) "Git")
-    (git-wip-mode t)))
+  (defun git-wip-mode-if-git ()
+    (when (string= (vc-backend (buffer-file-name)) "Git")
+      (git-wip-mode t)))
 
-(add-hook 'find-file-hook 'git-wip-mode-if-git)
+  (add-hook 'find-file-hook 'git-wip-mode-if-git)
 
-(provide 'git-wip-mode)
-;;; git-wip-mode.el ends here
+  (provide 'git-wip-mode)
+  ;;; git-wip-mode.el ends here
--- emacs/git-wip.el
+++ emacs/git-wip.el
@@ -1,4 +1,31 @@
-(defun git-wip-wrapper () 
+;; -*- mode: emacs-lisp; -*-
+
+
+(defun git-wip-push (arg)
+  (interactive "P")
+  (when (string= (vc-backend (buffer-file-name)) "Git")
+    (make-process :name "git-wip"
+                  :buffer git-wip-buffer-name
+                  :command (if arg
+                               (list git-wip-path "push" "-f" "--")
+                             (list git-wip-path "push" "--"))
+                  :sentinel #'(lambda (process event)
+                                "Sentinel to handle process exit status."
+                                (when (string-match-p "finished\\|exited" event)
+                                  (let ((exit-code (process-exit-status process)))
+                                    (message "event %s" event)
+                                    (message "exit-code %d" exit-code)
+                                    (if (zerop exit-code)
+                                        (message (concat "Wrote and git-wip-push'd "
+                                                         (buffer-file-name)))
+                                      (message "Process %s-push failed with exit code %d"
+                                               (process-name process)
+                                               exit-code)
+                                      (error "Process %s-push failed with exit code %d"
+                                             (process-name process)
+                                             exit-code))))))))
+
+(defun git-wip-wrapper ()
   (interactive)
   (let ((file-arg (shell-quote-argument (buffer-file-name))))
     (shell-command (concat "git-wip save \"WIP from emacs: " (buffer-file-name) "\" --editor -- " file-arg))
@@ -10,3 +37,7 @@
     (git-wip-wrapper)))
 
 (add-hook 'after-save-hook 'git-wip-if-git)
+
+
+(provide 'git-wip)
+
